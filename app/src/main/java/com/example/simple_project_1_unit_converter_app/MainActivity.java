a9aba625627b072ee6c006b65dd4aa6256b195a2
tristanbagnulo <tristanbagnulo1@gmail.com>
2022-01-05 16:12:09 +1100

    Simple_Project_1-Unit_Converter_App

---
+++ app/src/main/java/com/example/simple_project_1_unit_converter_app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.simple_project_1_unit_converter_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Java is an object-oriented language. So for any instance where in you need to use an asset or
+        //some interactive component, you need to "instantiate" it.
+        TextView title_unit_converter = (TextView) findViewById(R.id.title_unit_converter);
+        Button volumeBtn = (Button) findViewById(R.id.volumeBtn);
+        volumeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* System.out.println("Volume Button Clicked.");*/
+            }
+        });
+    }
+}
