152d529dcd902723963936af538eb9a6278c2e24
Tristan Bagnulo <tristan.bagnulo@optus.com.au>
2022-02-09 23:01:04 +1100

    sar

--- app/src/main/java/com/example/simple_project_1_unit_converter_app/VolumeConversionActivity.java
+++ app/src/main/java/com/example/simple_project_1_unit_converter_app/VolumeConversionActivity.java
@@ -11,4 +11,12 @@ public class VolumeConversionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume_conversion);
     }
+
+    // 1. Accept user input into any one field. When user
+    // clicks "translate" button do the following.
+    // 2. Identify which unit was input.
+        // 2.1. Perform translation to other units depending
+        // on which was input.
+        // 2.2 Assign output value of each unit to variables.
+    // 3. Update all input fields to display results
 }
